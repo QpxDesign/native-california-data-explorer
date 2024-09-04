4a9732fc97201e529fffe11501ed94603714f532
qpxdesign <qpxdesign@users.noreply.github.com>
2024-09-03 19:39:31 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useEffect, useState} from 'react';
 import './App.css';
+import mapboxgl from "mapbox-gl";
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicXBhdHdhcmRoYW4tb3h5IiwiYSI6ImNtMG43MnhsbTAwaWMyam9rd2o3aHE4amQifQ.UIjCbHbgQ80yyhl_Do6lIg";
+    const mapContainer = useRef(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+  const [lng, setLng] = useState(-119.4179);
+  const [lat, setLat] = useState(36.7783);
+  const [zoom, setZoom] = useState(5);
+
+  useEffect(() => {
+    if (map?.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current ?? "",
+      style: "mapbox://styles/qpatwardhan-oxy/cm0n74650020y01qv0lvs2ae0",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+    map.current.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+        showUserHeading: true,
+      })
+    );
+
+    map.current.addControl(new mapboxgl.NavigationControl());
+      map?.current?.on('load', function () {
+  map?.current?.getStyle()?.layers.map(function (layer) {
+        if (layer.type === 'symbol') {
+        map?.current?.setLayoutProperty(layer.id, 'visibility', 'none');
+    }
+    if (layer.id.indexOf('road') >= 0) {
+      map?.current?.setLayoutProperty(layer.id, 'visibility', 'none');
+    }
+        map?.current?.addLayer(
+        {
+          id: 'qpatwardhan-oxy.9wdqut7m',
+          type: 'fill',
+          source: 'urban-areas',
+          layout: {},
+          paint: {
+            'fill-color': '#f08',
+            'fill-opacity': 0.4
+          }
+        },
+
+      );
+  });
+  });
+
+});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="map-container" ref={mapContainer} />
     </div>
   );
 }
