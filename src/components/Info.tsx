d41a94832c574ed87057cfda68cf22799f089c44
qpxdesign <qpxdesign@users.noreply.github.com>
2024-09-04 20:45:38 -0700

    added section scroller, timeline

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import Timeline from "./Timeline"
+import InfoSections from "../assets/InfoSections.json"
+import { BsArrowDown } from "react-icons/bs";
+
+interface InfoSectionItem {
+  id: string;
+  index: number;
+  title: string;
+  text:string;
+  image_slug: string;
+  year: number;
+  sources: [string];
+}
 
 export default function Info() {
-  return (
-    <div className="info-container">
-      <h1>Californian Missions</h1>
-      <h2>See how missions transformed native california over time</h2>
-      <Timeline />
-  </div>
-  );
+  const [pos, setPos] = useState(0);
+
+
+  if (pos == 0) {
+    return (
+      <div className="info-container" onWheel={() => setPos(pos+1)}>
+        <h1>Californian Missions</h1>
+        <h2>See how missions transformed native california over time</h2>
+        <h5 style={{marginTop:"20em", fontSize:"1em", padding:0}}>Scroll Down</h5>
+        <BsArrowDown style={{fontSize:"3em", padding:0, margin: 0}}/>
+      <Timeline year={1590}/>
+    </div>
+    );
+  } else {
+    let  SectionInfo = InfoSections.find((a) => a.index === pos);
+    return (
+    <div className="info-container" onWheel={() => pos < InfoSections.length ? setPos(pos+1) : setPos(pos)}>
+      <img src={SectionInfo?.image_slug} style={{objectFit:"contain", width:"30em",marginRight:"auto"}}/>
+      <h1 style={{fontSize:"2.3rem",textAlign:"left",width:"95%"}}>{SectionInfo?.title ?? "error"}</h1>
+      <p style={{fontSize:"1.2rem",textAlign:"left",width:"95%"}}>{SectionInfo?.text ?? "error"}</p>
+
+        <Timeline year={1750}/>
+    </div>
+    )
+  }
+
 }
+
+//    <span className="full-center" style={{fontSize:"1.2em", }}><BsArrowDown style={{padding:0, margin: 0, marginRight: ".2em"}}/> Scroll Down</span>
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TimelineIcon from "../assets/TimelineIcon.svg"
 
-export default function Timeline() {
+interface TimelineProps {
+  year: number;
+}
+
+export default function Timeline(props: TimelineProps) {
+  const [year, setYear] = useState(props.year);
+  const MIN_YEAR = 1590;
+  const MAX_YEAR = 2024;
+  function scale (number : number, inMin : number, inMax : number, outMin : number, outMax : number) {
+    let a : Number = (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    return a;
+}
   return (
-    <div className="timeline-wrapper">
+    <div className="timeline-wrapper full-center">
       <span className="start_year">1590</span>
-      <input type="range" style={{width:"80%"}}/>
+      <div className="slider" style={{width:"90%", height:"1.5em", background: "white", borderRadius:"10em", margin: "1em", position:"relative", padding:0}}>
+        <div className="thumb"  style={{left: String(scale(year, MIN_YEAR, MAX_YEAR, 0, 100)) + "%"}}>
+        <img src={TimelineIcon} style={{background:"transparent"}} />
+        </div>
+      </div>
+
       <span className="start_year">2024</span>
     </div>);
 }
