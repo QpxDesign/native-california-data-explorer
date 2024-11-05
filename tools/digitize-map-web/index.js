497ec0f8c1d2967d5fb3dfd96266638382fbd19a
qpxdesign <qpxdesign@users.noreply.github.com>
2024-11-05 14:01:43 -0800

    added in lat/long pop-ups to map digitizer

--- tools/digitize-map-web/index.js
+++ tools/digitize-map-web/index.js
@@ -10,7 +10,7 @@ async function initMap() {
 
   const CENTER_LAT = 34.81040803396024 - 0.07; // LAT + OFFSET
   const CENTER_LONG = -119.8129875606221 + 0.09; // LONG + OFFSET
-
+  const centerLatLong = { lat: CENTER_LAT, lng: CENTER_LONG };
   // DO NOT TOUCH
   const HEIGHT = IMAGE_HEIGHT_PX / PIXELS_PER_MILE / 69;
   const WIDTH =
@@ -19,15 +19,15 @@ async function initMap() {
     (69 * Math.cos(CENTER_LAT * (Math.PI / 180)));
   const { Map } = await google.maps.importLibrary("maps");
   map = new Map(document.getElementById("map"), {
-    center: { lat: CENTER_LAT, lng: CENTER_LONG },
+    center: centerLatLong,
     zoom: 10,
   });
 
   console.log(
-    `SOUTHWEST CORNER: ${CENTER_LAT - HEIGHT / 2} ${CENTER_LONG - WIDTH / 2}`
+    `SOUTHWEST CORNER: ${CENTER_LAT - HEIGHT / 2} ${CENTER_LONG - WIDTH / 2}`,
   );
   console.log(
-    `NORTHEAST CORNER: ${CENTER_LAT + HEIGHT / 2} ${CENTER_LONG + WIDTH / 2}`
+    `NORTHEAST CORNER: ${CENTER_LAT + HEIGHT / 2} ${CENTER_LONG + WIDTH / 2}`,
   );
   const imageBounds = {
     north: CENTER_LAT + HEIGHT / 2,
@@ -38,10 +38,31 @@ async function initMap() {
 
   historicalOverlay = new google.maps.GroundOverlay(
     "./chumash.jpeg", // PATH_TO_MAP
-    imageBounds
+    imageBounds,
   );
   historicalOverlay;
-  historicalOverlay.setOpacity(0.5);
+  historicalOverlay.setOpacity(0.7);
   historicalOverlay.setMap(map);
+
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Click the map to get Lat/Lng!",
+    position: centerLatLong,
+  });
+
+  infoWindow.open(map);
+  historicalOverlay.addListener("click", (mapsMouseEvent) => {
+    // Close the current InfoWindow.
+    infoWindow.close();
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
+    });
+    infoWindow.setContent(
+      mapsMouseEvent.latLng.toJSON().lat +
+        "," +
+        mapsMouseEvent.latLng.toJSON().lng,
+    );
+    infoWindow.open(map);
+  });
 }
 initMap();
